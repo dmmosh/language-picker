55bc31e2e97ae3d0a68526daf7bce28aa627aabc
wettestsock <evilsharkeater@gmail.com>
2023-12-28 23:19:57 -0500

    nothing of note

--- main.py
+++ main.py
@@ -44,16 +44,16 @@ root = tk.Tk()
 
 root.geometry(str(b_height)+'x'+str(b_width))
 root.configure(bg=B)
-'''
+
 text = tk.Text(root, 
                height=b_height//2, 
                width=b_width, 
                background='black', 
                foreground='white',
                font=('Comic Sans', 20))
-'''
-#text.pack()
-#text.insert(tk.END, torch.zeros(4,2))
+
+text.pack()
+text.insert(tk.END, torch.zeros(4,2))
 
 canvas = tk.Canvas(root,
                    height=b_height,
@@ -64,6 +64,8 @@ canvas.pack()
 entry1 = tk.Entry(root, bg=B, fg=W)
 canvas.create_window(200,140, 
                      window= entry1)
+
+
 
 #input = entry1.get()
 
